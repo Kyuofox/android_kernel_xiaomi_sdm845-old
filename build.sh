c986cef7e878d0ade88705c723bf171e69160a3f
zclkkk <2484392714@qq.com>
2020-09-13 00:24:39 +0000

    build:fixs

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 rm -rf out
 export PATH=/home/zclkkk/clang/bin:${PATH}
-make O=out ARCH=arm64 dipper_defconfig
+make O=out ARCH=arm64 polaris_defconfig
 make -j$(nproc --all) O=out \ARCH=arm64 \CC=/home/zclkkk/clang/bin/clang \
 	CROSS_COMPILE=/home/zclkkk/aarch64-linux-gnu/bin/aarch64-linux-gnu- \
 	CROSS_COMPILE_ARM32=/home/zclkkk/arm-linux-gnueabi/bin/arm-linux-gnueabi- \
 	LD=/home/zclkkk/clang/bin/ld.lld \
 	OBJCOPY=/home/zclkkk/clang/bin/llvm-objcopy \
+	OBJDUMP=/home/zclkkk/clang/bin/llvm-objdump \
+	STRIP=/home/zclkkk/clang/bin/llvm-strip \
 	NM=/home/zclkkk/clang/bin/llvm-nm \
 	AR=/home/zclkkk/clang/bin/llvm-ar | tee kernel.log
