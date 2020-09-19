c89057be3282d82927faf5309096437e311ce8b9
zclkkk <69691993+zclkkk@users.noreply.github.com>
2020-09-19 10:55:17 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 rm -rf out
-export PATH=/home/zclkkk/clang/bin:${PATH}
+export PATH=/data/clang/bin:${PATH}
 make O=out ARCH=arm64 polaris_defconfig
-make -j$(nproc --all) O=out \ARCH=arm64 \CC=/home/zclkkk/clang/bin/clang \
-	CROSS_COMPILE=/home/zclkkk/aarch64-linux-gnu/bin/aarch64-linux-gnu- \
-	CROSS_COMPILE_ARM32=/home/zclkkk/arm-linux-gnueabi/bin/arm-linux-gnueabi- \
-	LD=/home/zclkkk/clang/bin/ld.lld \
-	OBJCOPY=/home/zclkkk/clang/bin/llvm-objcopy \
-	OBJDUMP=/home/zclkkk/clang/bin/llvm-objdump \
-	STRIP=/home/zclkkk/clang/bin/llvm-strip \
-	NM=/home/zclkkk/clang/bin/llvm-nm \
-	AR=/home/zclkkk/clang/bin/llvm-ar | tee kernel.log
+make -j$(nproc --all) O=out \ARCH=arm64 \CC=/data/clang/bin/clang \
+	CROSS_COMPILE=/data/clang/bin/aarch64-linux-gnu- \
+	CROSS_COMPILE_ARM32=/data/clang/bin/arm-linux-gnueabi- \
+	LD=/data/clang/bin/ld.lld \
+	OBJCOPY=/data/clang/bin/llvm-objcopy \
+	OBJDUMP=/data/clang/bin/llvm-objdump \
+	STRIP=/data/clang/bin/llvm-strip \
+	NM=/data/clang/bin/llvm-nm \
+	AR=/data/clang/bin/llvm-ar | tee kernel.log
