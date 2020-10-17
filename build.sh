6668b5c7e945dc154cb82e99b120df4175f3511c
zclkkk <2484392714@qq.com>
2020-10-17 13:56:54 +0000

    update build.sh

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@ rm -rf out
 export PATH=/data/clang/bin:${PATH}
 make O=out ARCH=arm64 polaris_defconfig
 make -j$(nproc --all) O=out \ARCH=arm64 \CC=/data/clang/bin/clang \
-	CROSS_COMPILE=/data/clang/bin/aarch64-linux-gnu- \
-	CROSS_COMPILE_ARM32=/data/clang/bin/arm-linux-gnueabi- \
+	CROSS_COMPILE=/data/aarch64-linux-gnu/aarch64-linux-gnu- \
+	CROSS_COMPILE_ARM32=/data/arm-linux-gnueabi/bin/arm-linux-gnueabi- \
 	LD=/data/clang/bin/ld.lld \
 	OBJCOPY=/data/clang/bin/llvm-objcopy \
 	OBJDUMP=/data/clang/bin/llvm-objdump \
